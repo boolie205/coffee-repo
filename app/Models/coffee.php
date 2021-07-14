b9e721b97c31e35e841fa60c46792af514fbf532
lewis <lewis@digitalvikings.uk>
2021-07-14 17:38:06 -0600

    re map array in more readable solution

--- app/Models/coffee.php
+++ app/Models/coffee.php
@@ -11,9 +11,22 @@ class coffee extends Model
     
     static public function GetCoffeesAndOptionsAsStrings(){
         $coffees = DB::table('coffee')->select('id','name_of_coffee','CoffeeOptions','created_at')->get();
+
+//this could be solved in a one liner but its really...really dirty.
+//but it looks cool so i will leave it in a comment
+//implode(" & ", array_flip(array_map(function($v) { global $mapIndex; return ++$mapIndex; }, array_filter(json_decode($coffee->CoffeeOptions,true), function($v) { return $v; }))))
+         function ReMapArray($arr) {
+            $ret = array();
+            foreach($arr as $k => $v) {
+                if($v) {
+                    $ret[] = $k;
+                }
+            }
+            return $ret;
+        }
+
         foreach($coffees as $coffee){
-            //decode coffee options, filter for only true values, flip the keys and values and then implode with & delimiter
-            $coffee->CoffeeOptions = implode(" & ", array_flip(array_map(function($v) { global $mapIndex; return ++$mapIndex; }, array_filter(json_decode($coffee->CoffeeOptions,true), function($v) { return $v; }))));
+            $coffee->CoffeeOptions = implode(" & ", ReMapArray(json_decode($coffee->CoffeeOptions,true)));
         }
 
         return $coffees;
